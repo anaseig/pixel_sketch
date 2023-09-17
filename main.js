f9765b655103e182b38369f0d5f078df95033c18
anaseig <70536934+anaseig@users.noreply.github.com>
2023-09-17 10:51:19 +0600

    v1.4 - improved logic

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let colorArrays = ["black", "rebeccapurple", "dodgerblue", "limegreen", "orange", "crimson"];
+let colorArrays = ["black", "rebeccapurple", "dodgerblue", "#4169e1", "limegreen", "#ffd800", "orange", "crimson"];
 let canvas = document.getElementById("canvas");
+let container = document.getElementById("container");
 
 
 //RESOLUTION LOGIC
@@ -17,53 +18,61 @@ function resolutionSquare(x) {
 
     //DIFFERENT CANVAS SIZE ON DIFFERENT RESOLUTIONS
     if (x == 16) {
-        let pixels = document.getElementsByClassName("pixel");
-        pixels.width = "15px";
-        pixels.height = "15px";
         canvas.style.width = "520px";
         canvas.style.height = "520px";
         canvas.style.minWidth = "520px";
         canvas.style.minHeight = "520px";
 
     } else if (x == 32) {
-        
-        /*let pixels = document.getElementsByClassName("pixel");
         canvas.style.width = "520px";
         canvas.style.height = "520px";
-        pixels.width = "15px";
-        pixels.height = "15px";
-        canvas.appendChild(pixels);*/
+        canvas.style.minWidth = "520px";
+        canvas.style.minHeight = "520px";
     }
-
 }
 
 resolutionSquare(16);
 
 
-
+let rainbowMode = false;
 let paintColor = "black";
 
+
+
 //DRAWING LOGIC
+let draw;
+
+container.addEventListener("mousedown", (e) => {
+    draw = true;
+    e.preventDefault();
+});
+container.addEventListener("mouseup", (e) => {
+    draw = false;
+});
+
 canvas.addEventListener("mouseover", function(e) {
+    
+    if (draw) {
 
-    e.target.style.backgroundColor = paintColor;
-    canvas.style.backgroundColor = "white";
+        if (rainbowMode == false) {
+            e.target.style.backgroundColor = paintColor;
+        } else {
+            e.target.style.backgroundColor = colorArrays[Math.floor(Math.random() * colorArrays.length)];
+        }
 
+    }
 
-    /*//CLICK TO ERASE THE COLOR
-    if (canvas.addEventListener("click", function(e){
-        e.target.style.backgroundColor = "white";
-    }));*/
-
+    
 });
 
 
 
-//COLOR
+//COLOR BUTTON
 let colorButton = document.getElementById("colorSelectButton");
 let i = 0;
 
 colorButton.addEventListener("click", function(e){
+    rainbowMode = false;
     i++;
 
     if (i >= colorArrays.length) {
@@ -89,13 +98,16 @@ colorButton.addEventListener("click", function(e){
 let eraserButton = document.getElementById("eraserButton");
 
 eraserButton.addEventListener("click", function(e){
+    i = -1;
+    rainbowMode = false;
     paintColor = "white";
-    //eraserButton.style.border = "10px solid black"
+    
     eraserButton.style.background = "rgb(255, 67, 67)";
     eraserButton.style.color = "white";
     
     colorButton.style.background = "white";
     colorButton.style.color = "black";
+
     rainbowButton.style.background = "white";
     rainbowButton.style.border = "none";
     rainbowButton.innerHTML = "rainbow";
@@ -108,16 +120,17 @@ eraserButton.addEventListener("click", function(e){
 let rainbowButton = document.getElementById("rainbowButton");
 
 rainbowButton.addEventListener("click", function(e){
-
-    let rainbowMode = 1;
+    i = -1;
+    rainbowMode = true;
 
     rainbowButton.style.cssText = "background: linear-gradient(90deg, rgba(255, 0, 0, 1) 0%,rgba(255, 154, 0, 1) 10%,rgba(208, 222, 33, 1) 20%,rgba(79, 220, 74, 1) 30%,rgba(63, 218, 216, 1) 40%,rgba(47, 201, 226, 1) 50%,rgba(28, 127, 238, 1) 60%,rgba(95, 21, 242, 1) 70%,rgba(186, 12, 248, 1) 80%,rgba(251, 7, 217, 1) 90%,rgba(255, 0, 0, 1) 100%);";
     rainbowButton.style.border = "10px solid black"
     rainbowButton.innerHTML = "";
 
-    paintColor = colorArrays[Math.floor(Math.random() * colorArrays.length)];
+
     colorButton.style.background = "white";
     colorButton.style.color = "black";
+
     eraserButton.style.background = "white";
     eraserButton.style.color = "black";
 });
